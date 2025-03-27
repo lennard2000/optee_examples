c596f5892271c842adab0ce7db6aba888ccfb73a
Lennard Gross <lennard@grotech.de>
2025-03-27 02:07:26 +0100

    add optee functions

--- hello_world/ta/hello_world_ta.c
+++ hello_world/ta/hello_world_ta.c
@@ -157,57 +157,23 @@ TEE_Result TA_CreateEntryPoint(void) {
     return TEE_SUCCESS;
 }
 
-/*
- * Called when the instance of the TA is destroyed if the TA has not
- * crashed or panicked. This is the last call in the TA.
- */
-void TA_DestroyEntryPoint(void)
-{
-	DMSG("has been called");
+void TA_DestroyEntryPoint(void) {}
+
+TEE_Result TA_OpenSessionEntryPoint(uint32_t param_types, TEE_Param params[4], void **sess_ctx) {
+    (void) param_types;
+    (void) params;
+    (void) sess_ctx;
+
+    psa_status_t status = psa_crypto_init();
+    if (status != PSA_SUCCESS) {
+        return TEE_ERROR_GENERIC;
+    }
+
+    return TEE_SUCCESS;
 }
 
-/*
- * Called when a new session is opened to the TA. *sess_ctx can be updated
- * with a value to be able to identify this session in subsequent calls to the
- * TA. In this function you will normally do the global initialization for the
- * TA.
- */
-TEE_Result TA_OpenSessionEntryPoint(uint32_t param_types,
-		TEE_Param __maybe_unused params[4],
-		void __maybe_unused **sess_ctx)
-{
-	uint32_t exp_param_types = TEE_PARAM_TYPES(TEE_PARAM_TYPE_NONE,
-						   TEE_PARAM_TYPE_NONE,
-						   TEE_PARAM_TYPE_NONE,
-						   TEE_PARAM_TYPE_NONE);
-
-	DMSG("has been called");
-
-	if (param_types != exp_param_types)
-		return TEE_ERROR_BAD_PARAMETERS;
-
-	/* Unused parameters */
-	(void)&params;
-	(void)&sess_ctx;
-
-	/*
-	 * The DMSG() macro is non-standard, TEE Internal API doesn't
-	 * specify any means to logging from a TA.
-	 */
-	IMSG("Hello World!\n");
-
-	/* If return value != TEE_SUCCESS the session will not be created. */
-	return TEE_SUCCESS;
-}
-
-/*
- * Called when a session is closed, sess_ctx hold the value that was
- * assigned by TA_OpenSessionEntryPoint().
- */
-void TA_CloseSessionEntryPoint(void __maybe_unused *sess_ctx)
-{
-	(void)&sess_ctx; /* Unused parameter */
-	IMSG("Goodbye!\n");
+void TA_CloseSessionEntryPoint(void *sess_ctx) {
+    (void) sess_ctx;
 }
 
 static TEE_Result inc_value(uint32_t param_types,
