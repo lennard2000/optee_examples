710a344485f009d48dff41171b84602ba1c48b83
Lennard Gross <lennard@grotech.de>
2025-03-27 02:06:49 +0100

    add psa code

--- hello_world/ta/hello_world_ta.c
+++ hello_world/ta/hello_world_ta.c
@@ -48,17 +48,113 @@
 #include <include/crypto_struct.h>
 
 
-#include <hello_world_ta.h>
+/* Information about hashing with the PSA API can be
+ * found here:
+ * https://arm-software.github.io/psa-api/crypto/1.1/api/ops/hashes.html
+ *
+ * The algorithm used by this demo is SHA 256.
+ * Please see include/psa/crypto_values.h to see the other
+ * algorithms that are supported by Mbed TLS.
+ * If you switch to a different algorithm you will need to update
+ * the hash data in the EXAMPLE_HASH_VALUE macro below. */
+
+/* Dummy inputs for HMAC */
+const unsigned char msg1_part1[] = { 0x01, 0x02 };
+const unsigned char msg1_part2[] = { 0x03, 0x04 };
+const unsigned char msg2_part1[] = { 0x05, 0x05 };
+const unsigned char msg2_part2[] = { 0x06, 0x06 };
+
+/* Dummy key material - never do this in production!
+ * This example program uses SHA-256, so a 32-byte key makes sense. */
+const unsigned char key_bytes[32] = { 0 };
+
+/* Print the contents of a buffer in hex */
+void print_buf(const char *title, uint8_t *buf, size_t len)
+{
+    printf("%s:", title);
+    for (size_t i = 0; i < len; i++) {
+        printf(" %02x", buf[i]);
+    }
+    printf("\n");
+}
+
+/* Run a PSA function and bail out if it fails.
+ * The symbolic name of the error code can be recovered using:
+ * programs/psa/psa_constant_name status <value> */
+#define PSA_CHECK(expr)                                       \
+    do                                                          \
+    {                                                           \
+        status = (expr);                                      \
+        if (status != PSA_SUCCESS)                             \
+        {                                                       \
+            printf("Error %d at line %d: %s\n",                \
+                   (int) status,                               \
+                   __LINE__,                                   \
+                   #expr);                                    \
+            goto exit;                                          \
+        }                                                       \
+    }                                                           \
+    while (0)
 
 /*
- * Called when the instance of the TA is created. This is the first call in
- * the TA.
+ * This function demonstrates computation of the HMAC of two messages using
+ * the multipart API.
  */
-TEE_Result TA_CreateEntryPoint(void)
+psa_status_t hmac_demo(void)
 {
-	DMSG("has been called");
+    psa_status_t status;
+    const psa_algorithm_t alg = PSA_ALG_HMAC(PSA_ALG_SHA_256);
+    uint8_t out[PSA_MAC_MAX_SIZE]; // safe but not optimal
+    /* PSA_MAC_LENGTH(PSA_KEY_TYPE_HMAC, 8 * sizeof( key_bytes ), alg)
+     * should work but see https://github.com/Mbed-TLS/mbedtls/issues/4320 */
 
-	return TEE_SUCCESS;
+    psa_key_attributes_t attributes = PSA_KEY_ATTRIBUTES_INIT;
+    psa_key_id_t key = 0;
+
+    /* prepare key */
+    // we derive the usage from the key alg, so we don't need this
+
+    // psa_set_key_usage_flags(&attributes, PSA_KEY_USAGE_SIGN_MESSAGE);
+    // psa_set_key_algorithm(&attributes, alg);
+    // psa_set_key_type(&attributes, PSA_KEY_TYPE_HMAC);
+    // psa_set_key_bits(&attributes, 8 * sizeof(key_bytes));     // optional
+
+    status = psa_generate_key(*key_bytes, alg, &key);
+    if (status != PSA_SUCCESS) {
+        return status;
+    }
+
+    /* prepare operation */
+
+    // again no initialisation needed
+    psa_mac_operation_t* op;
+    size_t out_len = 0;
+
+    /* compute HMAC(key, msg1_part1 | msg1_part2) */
+    PSA_CHECK(psa_mac_sign_setup(&op, key, &alg));
+    PSA_CHECK(psa_mac_update(&op, msg1_part1, sizeof(msg1_part1)));
+    PSA_CHECK(psa_mac_update(&op, msg1_part2, sizeof(msg1_part2)));
+    PSA_CHECK(psa_mac_sign_finish(&op, out, sizeof(out), &out_len));
+    print_buf("msg1", out, out_len);
+
+    /* compute HMAC(key, msg2_part1 | msg2_part2) */
+    PSA_CHECK(psa_mac_sign_setup(&op, key, &alg));
+    PSA_CHECK(psa_mac_update(&op, msg2_part1, sizeof(msg2_part1)));
+    PSA_CHECK(psa_mac_update(&op, msg2_part2, sizeof(msg2_part2)));
+    PSA_CHECK(psa_mac_sign_finish(&op, out, sizeof(out), &out_len));
+    print_buf("msg2", out, out_len);
+
+exit:
+    // not needed, since we clear automatically
+    // psa_mac_abort(&op);   // needed on error, harmless on success
+    // psa_destroy_key(key);
+    mbedtls_platform_zeroize(out, sizeof(out));
+
+    return status;
+}
+
+TEE_Result TA_CreateEntryPoint(void) {
+    return TEE_SUCCESS;
 }
 
 /*
